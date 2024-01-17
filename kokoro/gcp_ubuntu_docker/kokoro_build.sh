4b291e72468bdbda81640b354ee660499dfcffd8
Jerin John <jerinjohn@google.com>
2024-01-17 13:19:47 -0800

    Updated target dir name in kokoro build config and script

--- kokoro/gcp_ubuntu_docker/kokoro_build.sh
+++ kokoro/gcp_ubuntu_docker/kokoro_build.sh
@@ -15,5 +15,5 @@ set -e
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
-cd "${KOKORO_ARTIFACTS_DIR}/github/tableau-connector
+cd "${KOKORO_ARTIFACTS_DIR}/github/looker-tableau-connector"
 ./build.sh
