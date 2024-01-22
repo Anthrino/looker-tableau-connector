5e65e48abcea5cc4df52bb3d6f5a7a6f0aedce27
Jerin John <jerinjohn@google.com>
2024-01-22 15:40:11 -0800

    Corrected file paths in taco sign script

--- sign.sh
+++ sign.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Move to packaged TACO file location 
-cd $KOKORO_PIPER_DIR/github/looker-tableau-connector/connector-plugin-sdk/connector-packager/packaged-connector
+cd "${KOKORO_GFILE_DIR}/github/looker-tableau-connector/connector-plugin-sdk/connector-packager/packaged-connector"
 
 # Create signed/ dir to store signed TACO file
 mkdir -p signed && chmod 777 signed
 
-#/escalated_sign/escalated_sign.py -j /escalated_sign_jobs -t signjar \
-# $KOKORO_PIPER_DIR/github/looker-tableau-connector/connector-plugin-sdk/connector-packager/packaged-connector
-# $KOKORO_PIPER_DIR/github/looker-tableau-connector/connector-plugin-sdk/connector-packager/packaged-connector
+echo "Executing jarsigner.."
+/escalated_sign/escalated_sign.py -j /escalated_sign_jobs -t signjar \
+"${KOKORO_GFILE_DIR}/github/looker-tableau-connector/connector-plugin-sdk/connector-packager/packaged-connector/" \
+"${KOKORO_GFILE_DIR}/github/looker-tableau-connector/connector-plugin-sdk/connector-packager/packaged-connector/signed"
+
+echo "Created signed TACO file:"
+ls "${KOKORO_GFILE_DIR}/github/looker-tableau-connector/connector-plugin-sdk/connector-packager/packaged-connector/signed"
